4889720a805a77ca003b360e2a92fb212887b4d9
Aleksandr L <bossstartov57@gmail.com>
2020-10-04 21:37:17 +0200

    Register models on admin and add tollbar + ckeditor

--- siteblog/siteblog/urls.py
+++ siteblog/siteblog/urls.py
@@ -19,9 +19,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adm/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('blog.urls')),
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
