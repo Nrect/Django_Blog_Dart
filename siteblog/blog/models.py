8c0c1245250a3b3766f6e342740bf0597f1d5ec5
Aleksandr L <bossstartov57@gmail.com>
2020-10-05 15:03:04 +0200

    Update admin

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -23,6 +23,8 @@ class Category(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Категория(ю)'
+        verbose_name_plural = 'Категории'
         ordering = ['title']
 
 
@@ -34,22 +36,26 @@ class Tag(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
         ordering = ['title']
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255,verbose_name='Заголовок')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
-    author = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
+    author = models.CharField(max_length=100,verbose_name='Автор')
+    content = models.TextField(blank=True,verbose_name='Содержание')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, null=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, null=True,verbose_name='Фото')
     views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField(Tag, blank=True, related_name='tags')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts',verbose_name='Категория')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='tags',verbose_name='Тег')
 
     def __str__(self):
         return self.title
 
     class Meta:
+        verbose_name = 'Статя'
+        verbose_name_plural = 'Статью'
         ordering = ['-created_at']
