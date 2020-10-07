c0fd5bfa308dbc3b6927353a3d72ee5d610367bf
Aleksandr L <bossstartov57@gmail.com>
2020-10-07 21:30:25 +0200

    Create single page and tag_list

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from blog.models import Post, Category, Tag
+from django.db.models import F
 
 
 class Home(ListView):
@@ -31,5 +32,30 @@ class PostByCategory(ListView):
         return context
 
 
-def get_post():
-    pass
+class GetPost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        # перезапросить данные из бд, чтобы выражение не было таким F(views) + Value(1)
+        self.object.refresh_from_db()
+        return context
+
+
+class PostByTag(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
